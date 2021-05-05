5f525ff2dd8b0f02c275b0cb95cc562a1adba9a7
Aapo Poutanen <aapo.poutanen@gmail.com>
2021-05-05 18:38:12 +0300

    add html

--- poestoori.go
+++ poestoori.go
@@ -6,9 +6,35 @@ import (
 	"poegen"
 )
 
+var (
+	html string = `<html>
+<head>
+	<style>
+		body {
+			padding: 2em;
+			max-width: 600px;
+			font-family: sans-serif;
+		}
+
+		div {
+			margin-bottom: 3em;
+		}
+	</style>
+</head>
+<body>
+	<h1>Poestoori</h1>
+	<div>%s</div>
+	<a href="/">Uus stoori :D</a>
+</body>
+<footer>
+  <p>HTML from Tomi Björckin äijästoori</p>
+	</footer> 
+</html>`
+)
+
 func serverecipe(w http.ResponseWriter, req *http.Request) {
 
-	fmt.Fprintf(w, poegen.Generate())
+	fmt.Fprintf(w, fmt.Sprintf(html, poegen.Generate()))
 }
 
 func main() {
